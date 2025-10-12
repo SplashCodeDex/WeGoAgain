1284d4cdb0661fdcfe001b0354b52a874410688e
splashdexstudios <splashdexstudios@gmail.com>
2025-10-12 03:28:10 +0000

    feat: Replace custom AvatarCard with FAvatar.raw

--- Todo.md
+++ Todo.md
@@ -2,14 +2,18 @@
 
 **Date:** 2025-10-12
 
-**Version:** 1.2.1
+**Version:** 1.3.1
 
-## Implemented Changes (v1.2.0 -> v1.2.1)
+## Implemented Changes (v1.3.0 -> v1.3.1)
 
-- [x] Removed "Find motivational books on Amazon" (AffiliateLink) feature.
-- [x] Removed "Sponsor a Quote" feature.
+- [x] Integrated Forui UI library.
+- [x] Updated `main.dart` to use `FAnimatedTheme` and `FThemes`.
+- [x] Updated `home_page.dart` to use `FScaffold`, `FAppBar`, `FButton`, and `FText`.
+- [x] Updated `achievements_page.dart` to use `FScaffold` and `FText`.
+- [x] Updated `leaderboard_page.dart` to use `FScaffold` and `FText`.
+- [x] Replaced custom `AvatarCard` with `FAvatar.raw`.
 
-## Upcoming Features (v1.3.0)
+## Upcoming Features (v1.4.0)
 
 - [ ] **Quote Categories:**
     - [ ] Introduce categories like "Stoicism," "Mindfulness," and "Grit" to allow users to tailor the content to their interests.
--- lib/widgets/avatar_card.dart
+++ lib/widgets/avatar_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:forui/forui.dart';
 
 class AvatarCard extends StatelessWidget {
   const AvatarCard({
@@ -16,25 +17,11 @@ class AvatarCard extends StatelessWidget {
       onTap: () {
         HapticFeedback.lightImpact();
       },
-      child: Container(
-        width: 60,
-        height: 60,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 10,
-              offset: const Offset(0, 5),
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: SvgPicture.asset(
-            'assets/images/avatar${index + 1}.svg',
-            fit: BoxFit.cover,
-          ),
+      child: FAvatar.raw(
+        size: 60,
+        child: SvgPicture.asset(
+          'assets/images/avatar${index + 1}.svg',
+          fit: BoxFit.cover,
         ),
       ),
     );
